74db470cf74419a829b61fadf20f24565e1f72d0
Harry <user@es302n24>
2018-10-02 11:06:36 +0800

    exam

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		int age = 18;
+		float weight = 55.4f;
+		float high = 176.9f;
+		String scoolYear = "2018";
+		System.out.print("age=");
+		System.out.println(age=18);
+		System.out.println("weight=55.4");
+		System.out.println("high=176.9 ");
+		System.out.println("scoolYear=2018");
+		
+		
 	}
 
 }
