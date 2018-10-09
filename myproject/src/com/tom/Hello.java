c158802740b03ce77d2091ca9306705298b8ac5f
user <user@es302n16>
2018-10-09 12:03:42 +0800

    second

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -4,18 +4,23 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		int age = 18;
-		float weight = 55.4f;
-		float high = 176.9f;
-		String scoolYear = "2018";
-		System.out.print("age=");
-		System.out.println(age=18);
-		System.out.println("weight=55.4");
-		System.out.println("high=176.9 ");
-		System.out.println("scoolYear=2018");
-		
-		
+//		System.out.println("Hello World!");
+//		int age = 18;
+//		float weight = 55.4f;
+//		float height = 1.7f;
+//		String scoolYear = "2018";
+//		System.out.print("age=");
+//		System.out.println(age=18);
+//		System.out.println("weight=55.4");
+//		System.out.println("height=1.7 ");
+//		System.out.println("scoolYear=2018");\*
+		Person p = new Person(55.4f, 1.7f);
+//		p.weight = 55.4f;
+//		p.height = 1.7f;
+		System.out.println(p.bmi());
+		p.hello();
+//		Person Harry =new Person();
+//		Person jack = null;
 	}
 
 }
